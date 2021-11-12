5f722f84d50cd8f7690a809eb5fd2385ab536423
João Salomão <joaosalomao.613@gmail.com>
2021-11-12 11:15:32 -0300

    Implementando CRUD dos Todos com graphql

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,17 +1,59 @@
 import { Application } from 'express';
 import { buildSchema } from 'graphql';
 import { graphqlHTTP } from 'express-graphql';
-// import Database from './database';
+import Database from './database';
+
+interface Todo {
+  id: string;
+  title: string;
+  description: string;
+}
 
 export default function (app: Application) {
   const schema = buildSchema(`
+    type Todo {
+      id: ID
+      title: String
+      description: String
+    }
+
+    input InTodo {
+      title: String
+      description: String
+    }
+
     type Query {
-      hello: String
+      all: [Todo]
+      find(id: ID!): Todo
+    }
+
+    type Mutation {
+      create(input: InTodo): Todo
+      update(id: ID!, input: InTodo): Todo
+      delete(id: ID!): Boolean
     }
   `);
 
   const rootValue = {
-    hello: () => "Hello world",
+    all(): Todo[] {
+      return Database.all();
+    },
+
+    find(input: { id: string }) {
+      return Database.findById(input.id);
+    },
+
+    create(input: { title: string, description: string }): Todo {
+      return Database.add(input);
+    },
+
+    update(input: { id: string, title: string, description: string }): Todo | undefined {
+      return Database.update(input.id, input);
+    },
+
+    delete(input: { id: string }): boolean {
+      return Database.remove(input.id);
+    }
   };
 
   app.use(
